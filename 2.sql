10a49fb2b62bf3b558a34b7a2aee4f0a1d96fd68
Paulo Köch <paulo.koch@gmail.com>
2022-12-20 01:22:27 +0000

    More usings

--- 2.sql
+++ 2.sql
@@ -1,9 +1,9 @@
 select distinct c.*
 from customers c
 join orders o using(customerid)
-join orders_items oi1 on(oi1.orderid = o.orderid)
+join orders_items oi1 using(orderid)
 join products p1 on(p1.sku = oi1.sku)
-join orders_items oi2 on(oi2.orderid = o.orderid)
+join orders_items oi2 using(orderid)
 join products p2 on(p2.sku = oi2.sku)
 where true
     and p1.desc ilike '%coffee%'
