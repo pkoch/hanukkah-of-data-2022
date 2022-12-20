8dd4dcce269b5164e8abfe54478417704e637d93
Paulo Köch <paulo.koch@gmail.com>
2022-12-20 00:37:40 +0000

    2

---
+++ 2.sql
@@ -0,0 +1,12 @@
+select distinct c.*
+from customers c
+join orders o on(o.customerid::integer = c.customerid)
+join orders_items oi1 on(oi1.orderid = o.orderid)
+join products p1 on(p1.sku = oi1.sku)
+join orders_items oi2 on(oi2.orderid = o.orderid)
+join products p2 on(p2.sku = oi2.sku)
+where true
+    and p1.desc ilike '%coffee%'
+    and p2.desc ilike '%bagel%'
+    and c.name like 'J%'
+    and c.name like '%D%'
