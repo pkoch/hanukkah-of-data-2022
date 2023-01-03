f06267c20886c8a603aac3d92c196f65d35a3299
Paulo Köch <paulo.koch@gmail.com>
2023-01-03 19:56:59 +0000

    7

---
+++ 7.sql
@@ -0,0 +1,16 @@
+-- Got two results. 315-618-5263 worked.
+select c2.*
+from customers c1
+join orders o1 using(customerid)
+join orders_items oi1 using(orderid)
+join products  p1 using(sku)
+join products p2 on(
+    trim(substring(p2.desc from '[^(]+')) = trim(substring(p1.desc, '[^(]+'))
+    and p1.sku != p2.sku
+)
+join orders_items oi2 on(oi2.sku = p2.sku)
+join orders o2 on(o2.orderid = oi2.orderid AND o2.ordered::date = o1.ordered::date)
+join customers c2 on(c2.customerid = o2.customerid)
+where c1.customerid = 8342
+  and p1.sku like 'HOM%'  -- educated guess it was not a collectible.
+;
